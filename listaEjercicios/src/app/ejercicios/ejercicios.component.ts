640ccfe6647c90233fe5380b5623790adc5e773f
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-02-16 20:30:16 -0300

    cree un servicio y lo uni al componente

--- listaEjercicios/src/app/ejercicios/ejercicios.component.ts
+++ listaEjercicios/src/app/ejercicios/ejercicios.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ejercicio } from '../ejercicio';
-import { EJERCICIOS } from '../collection-ejercicios';
+//PASA A EJERCICIO.SERVICE.TS = import { EJERCICIOS } from '../collection-ejercicios';
+import { EjercicioService } from '../ejercicio.service';
 
 @Component({
   selector: 'app-ejercicios',
@@ -9,11 +10,29 @@ import { EJERCICIOS } from '../collection-ejercicios';
 })
 export class EjerciciosComponent implements OnInit {
 
-  ejercicios=EJERCICIOS;
-  ejercicioSeleccionado:Ejercicio;
-  constructor() { }
+  //ejercicios=EJERCICIOS;
 
+  ejercicios:Ejercicio[];
+  ejercicioSeleccionado:Ejercicio;
+  constructor(private ejercicioService:EjercicioService) { }
+
+  /***
+   * ejercicio:Ejercicio[] = creo el tipo de objeto ejercicio
+   * getEjercicios():void = no devuelve nada
+   *  this.ejercicios= donde voy a recibir los datos del servicio
+   *  this.ejercicioService.getEjercicios();= Desde donde se obtienen los ejercicios
+   */
+  getEjercicios():void{
+    this.ejercicios=this.ejercicioService.getEjercicios();
+  }
+ /**
+  *  ngOnInit(): void= donde se utilizan los metodos con this
+  *
+    this.getEjercicios(); = utilizo el metodo getEjercicios
+  */
   ngOnInit(): void {
+    this.getEjercicios();
+
   }
   onSelectEjercicio(ejercicio:Ejercicio):void{
     console.log("Ejercicio seleccionado="+ejercicio.id);
