e635484823a9a6f343c33b4bdacde4bfc568b42a
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-02-17 16:36:37 -0300

    Creo Ruta ejercicios

--- listaEjercicios/src/app/app-routing.module.ts
+++ listaEjercicios/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EjerciciosComponent } from './ejercicios/ejercicios.component';
 
-
-const routes: Routes = [];
+/**
+ * DEFINIMOS RUTAS
+ * path: 'ejercicios': Nombre de Ruta.
+ *  component:EjerciciosComponent: componente que se encarga de visualizar el template.
+ * Esto permite:http://localhost:4200/ejercicios
+ *
+ */
+const routes: Routes = [
+  {path: 'ejercicios', component:EjerciciosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+/***
+ * CREO RUTAS
+ */
