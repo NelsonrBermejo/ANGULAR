22a041c2fbfeef07f2d14e066ad41a01f44183eb
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-02-17 16:20:18 -0300

    creo Metodo asincrono para Observable de RxJS

--- listaEjercicios/src/app/ejercicios/ejercicios.component.ts
+++ listaEjercicios/src/app/ejercicios/ejercicios.component.ts
@@ -18,15 +18,27 @@ export class EjerciciosComponent implements OnInit {
     console.log("-----Componente Ejercicios CREADO---");
    }
 
-  /***
+  /***METODO SINCRONO***************************************
    * ejercicio:Ejercicio[] = creo el tipo de objeto ejercicio
    * getEjercicios():void = no devuelve nada
    *  this.ejercicios= donde voy a recibir los datos del servicio
    *  this.ejercicioService.getEjercicios();= Desde donde se obtienen los ejercicios
    */
+  //getEjercicios_old():void{
+  //  this.ejercicios=this.ejercicioService.getEjercicios();
+  //}
+
+  /**METODO ASINCRONO*******
+   * this.ejercicioService.getEjercicios(): utilizo la funcion getEjercicios
+   * a quien le solicito la coleccion de ejercicios.
+   * .suscribe(): es por el modelo, o filosofia de RxJS.
+   * ejercicios: se define el tipo en ejercicios:Ejercicio[].
+   * =>this.ejercicios=ejercicios: paso la coleccion de ejercicios a ejercicios.
+   */
   getEjercicios():void{
-    this.ejercicios=this.ejercicioService.getEjercicios();
+    this.ejercicioService.getEjercicios().subscribe(ejercicios=>this.ejercicios=ejercicios)
   }
+
  /**
   *  ngOnInit(): void= donde se utilizan los metodos con this
   *
